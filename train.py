7e6fd0293dafe667b7d18af2ae86a896cd5038bb
hotsoycandy <wkdwnsghd617@gmail.com>
2023-09-24 20:19:59 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,33 @@
-print("I'm VGGNet!")
+"""VGG Train"""
+
+from matplotlib import pyplot as plt
+import numpy as np
+## torches
+import torch
+from torch import nn
+from torchvision import transforms as torchTrans
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from torchsummary import summary as torchsummary
+
+def get_device ():
+    """
+    get torch processing device
+    """
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+
+    return device
+
+device = get_device()
+print(device)
+
+def train ():
+    pass
+
+train()
